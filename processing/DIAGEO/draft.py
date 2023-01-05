8369b687c0bdda1d9a4910fdebf267b75a4409e0
ManoelDiageo <manoel.netto1@diageo.com>
2023-01-05 10:13:52 -0600

    Reorganization of files

---
+++ processing/DIAGEO/draft.py
@@ -0,0 +1,1 @@
+print('Emission factors')
--- variables/setup/file.py
+++ variables/setup/file.py
@@ -1,13 +1,14 @@
 import variables.var_column as clmn
 
-filepath = 'C:/Users/emily/PycharmProjects/PersonalPNL/inputs/setup.xlsx'
+# filepath = 'C:/Users/emily/PycharmProjects/PersonalPNL/inputs/setup.xlsx'
+filepath = 'C:/Users/NettoMan1/OneDrive - Diageo/Category files/99. Python/spreadsheetGrinder/inputs/DIAGEO/setup.xlsx'
 setup_raw_sheet_name = 'setup RAW'
 setup_mtx_sheet_name = 'setup MTX'
 
 column_sheet_name = 'column'
-root_folder = 'C:/Users/emily/PycharmProjects/PersonalPNL'
-json_folder = 'outputs/json'
-dataframe_folder = 'outputs/dataframe'
+root_folder = 'C:/Users/NettoMan1/OneDrive - Diageo/Category files/99. Python/spreadsheetGrinder'
+json_folder = 'outputs/DIAGEO/json'
+dataframe_folder = 'outputs/DIAGEO/dataframe'
 csv = 'csv'
 json = 'json'
 json_indent = 4
